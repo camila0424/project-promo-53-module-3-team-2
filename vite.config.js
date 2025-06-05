782131898890b3dc02f2943c473d3e1876f7a811
Roxana Solano <roxansolan@gmail.com>
2025-06-05 21:50:10 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     open: "/",
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+  },
+  base: "/project-promo-53-module-3-team-2/",
 });
